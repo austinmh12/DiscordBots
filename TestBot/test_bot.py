61d29c9284aa1f320652f1254ae6f84820d80a18
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-04-29 11:21:16 -0400

    Prepping for anime bot

--- TestBot/test_bot.py
+++ TestBot/test_bot.py
@@ -10,7 +10,7 @@ import typing
 from os.path import getmtime
 # from test_cogs.movie_poll import Movies
 # from test_cogs.attachments import AttachmentHandler
-from test_cogs.pokeroulette import PokeRoulette
+# from test_cogs.pokeroulette import PokeRoulette
 
 with open('../.env') as f:
 	ENV = {l.strip().split('=')[0]: l.strip().split('=')[1] for l in f.readlines()}
@@ -99,5 +99,5 @@ async def check_for_changes():
 
 client.loop.create_task(check_for_changes())
 # client.add_cog(Movies(client))
-client.add_cog(PokeRoulette(client))
+# client.add_cog(PokeRoulette(client))
 client.run(ENV['TESTBOT'], bot=True)
