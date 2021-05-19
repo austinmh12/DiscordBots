f4d8afafa6af194a1a28ecc620ebc40d93a9af6c
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-05-19 16:09:54 -0400

    Database intialisation placeholders

--- TestBot/test_cogs/rpg.py
+++ TestBot/test_cogs/rpg.py
@@ -4,6 +4,7 @@ from discord.ext import commands, tasks
 import asyncio
 from random import randint
 import typing
+import os.path
 
 # Version
 version = '0.0.0'
@@ -11,7 +12,13 @@ version = '0.0.0'
 # Constants
 
 # Functions
+def initialise_db():
+	...
 
 # Classes
 class RPGCog(MyCog):
-	...
+	def __init__(self, bot):
+		super().__init__(bot)
+		if not os.path.exists(f'{BASE_PATH}/rpg.db'):
+			log.info('Initialising database.')
+			initialise_db()
