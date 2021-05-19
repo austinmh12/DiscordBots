b9237c7a6176e5691e6d9ec7100d84e631038376
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-05-19 17:13:42 -0400

    Database initialisation

--- TestBot/test_cogs/rpg.py
+++ TestBot/test_cogs/rpg.py
@@ -13,7 +13,86 @@ version = '0.0.0'
 
 # Functions
 def initialise_db():
-	...
+	sql('rpg', 'create table players (id integer, guild_id integer, current_character text)')
+	sql('rpg', '''create table professions (
+			name text
+			,primary_stat text
+			,secondary_stat text
+			,base_str integer
+			,base_dex integer
+			,base_int integer
+			,base_con integer
+			,str_mod integer
+			,dex_mod integer
+			,int_mod integer
+			,con_mod integer
+			,starting_weapon integer
+			,starting_off_hand integer
+		)'''
+	)
+	sql('rpg', '''create table characters (
+			player_id integer
+			,player_guild_id integer
+			,name text
+			,profession text
+			,level integer
+			,exp integer
+			,exp_to_next_level integer
+			,gold integer
+			,helmet integer
+			,chest integer
+			,legs integer
+			,boots integer
+			,gloves integer
+			,amulet integer
+			,ring1 integer
+			,ring2 integer
+			,weapon integer
+			,off_hand integer
+			,current_con integer
+		)'''
+	)
+	sql('rpg', '''create table monsters (
+			name text
+			,primary_stat text
+			,secondary_stat text
+			,min_damage integer
+			,max_damage integer
+			,crit_chance integer
+			,base_str integer
+			,base_dex integer
+			,base_int integer
+			,base_con integer
+			,str_mod integer
+			,dex_mod integer
+			,int_mod integer
+			,con_mod integer
+			,loot_table text
+			,base_exp integer
+			,exp_mod integer
+		)'''
+	)
+	sql('rpg', '''create table players (
+			id integer
+			,name text
+			,rarity text
+			,type text
+			,level integer
+			,str_bonus integer
+			,dex_bonus integer
+			,int_bonus integer
+			,con_bonus integer
+			,def_bonus integer
+			,atk_bonus integer
+			,weight text
+			,defense integer
+			,min_damage integer
+			,max_damage integer
+			,stat text
+			,crit_chance integer
+		)'''
+	)
+	sql('rpg', 'create table areas (name text, recommended_level integer, monsters text)')
 
 # Classes
 class RPGCog(MyCog):
