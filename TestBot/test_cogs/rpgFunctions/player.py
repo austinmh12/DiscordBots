bd103af846996ff6d04b9c491026374b534e8888
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-05-20 08:36:08 -0400

    Player database functions

--- TestBot/test_cogs/rpgFunctions/player.py
+++ TestBot/test_cogs/rpgFunctions/player.py
@@ -9,10 +9,15 @@ from .. import sql, log, BASE_PATH, chunk
 # Functions #
 #############
 def get_player(id, guild_id):
-	...
+	df = sql('rpg', 'select * from players where id = ? and guild_id = ?', (id, guild_id))
+	if df.empty:
+		return None
+	return Player(**df.to_dict('records')[0])
 
 def add_player(id, guild_id):
-	...
+	player = Player(id, guild_id)
+	sql('rpg', 'insert into players values (?,?,?)', (player.id, player.guild_id, ''))
+	return player
 
 ###########
 # Classes #
@@ -21,7 +26,7 @@ class Player:
 	def __init__(self,
 				id,
 				guild_id,
-				current_character=None
+				current_character=''
 	):
 		self.id = id
 		self.guild_id = guild_id
