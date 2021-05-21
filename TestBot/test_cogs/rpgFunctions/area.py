31abdac9d9f3458f525175e907a0fb56ed8019c2
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-05-21 09:03:58 -0400

    Loot generation from area

--- TestBot/test_cogs/rpgFunctions/area.py
+++ TestBot/test_cogs/rpgFunctions/area.py
@@ -2,6 +2,7 @@ from .. import sql, log, BASE_PATH, chunk, Page
 from random import randint, random, choice
 import json
 from .monster import get_monster
+from .equipment import generate_random_equipment
 
 #############
 # Constants #
@@ -40,4 +41,13 @@ class Area:
 		return monster
 
 	def get_random_loot(self):
-		...
+		items = []
+		for _ in range(self.loot_table['max_item_count']):
+			if random() < self.loot_table['item_chance']:
+				item_type = choice(list(self.loot_table['items'].keys()))
+				item_info = self.loot_table[item_type]
+				rarity = choice(item_info['rarities'])
+				level = randint(item_info['min_level'], item_info['max_level'])
+				item = generate_random_equipment(item_type, rarity, level)
+				items.append(items)
+		return items
