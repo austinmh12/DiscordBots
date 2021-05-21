05162fb2ed30d7bd4c6c7d8689b031bce8e918b4
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-05-21 15:31:26 -0400

    Combat class holds current combat information

--- TestBot/test_cogs/rpgFunctions/combat.py
+++ TestBot/test_cogs/rpgFunctions/combat.py
@@ -14,10 +14,24 @@ from .. import sql, log, BASE_PATH, chunk, Page
 # Classes #
 ###########
 class Combat:
-	def __init__(self, area, enemies, characters):
+	def __init__(self, area, character):
 		self.area = area
-		self.enemies = [self.area.get_random_monster() for _ in range(enemies)]
-		self.characters = characters
+		self.character = character
+		self.enemy = self.area.get_random_monster()
+		self.winner = None
 
-	def do_combat(self):
+	def alive(self, entity):
+		return entity.current_con > 0
+
+	def character_combat(self, action):
+		if action == 'Attack':
+			...
+		elif action == 'Pass':
+			...
+		if self.alive(self.enemy):
+			self.enemy_combat()
+		else:
+			self.winner = self.character
+
+	def enemy_combat(self):
 		...
