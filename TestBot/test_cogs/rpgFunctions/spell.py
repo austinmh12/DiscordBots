8cc5b239a6d327ca12b74ab6ccaf5f99e79962c6
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-05-24 16:36:51 -0400

    Initial spell work

---
+++ TestBot/test_cogs/rpgFunctions/spell.py
@@ -0,0 +1,36 @@
+from .. import sql, log, BASE_PATH, chunk, Page
+from . import *
+from discord import Embed, Colour
+from random import randint, random
+
+#############
+# Constants #
+#############
+
+
+#############
+# Functions #
+#############
+def get_spells():
+	df = sql('rpg', 'select * from spells')
+	if df.empty:
+		return []
+	return [Spell(**d) for d in df.to_dict('records')]
+
+def get_spell(name):
+	df = sql('rpg', 'select * from spells where lower(name) = ?', (name.lower(),))
+	if df.empty:
+		return None
+	return Spell(**df.to_dict('records')[0])
+
+###########
+# Classes #
+###########
+class Spell:
+	def __init__(self, name, profession, level, min_damage, max_damage, stat):
+		self.name = name
+		self.profession = profession
+		self.level = level
+		self.min_damage = min_damage
+		self.max_damage = max_damage
+		self.stat = stat
