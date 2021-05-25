b3f9f38c4d807a5edd7fc380f2cf1a0e5e13f275
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-05-25 08:32:11 -0400

    Added spell details

--- TestBot/test_cogs/rpgFunctions/spell.py
+++ TestBot/test_cogs/rpgFunctions/spell.py
@@ -41,3 +41,14 @@ class Spell:
 		self.max_damage = max_damage
 		self.stat = stat
 		self.cost = cost
+
+	@property
+	def avg_dmg(self):
+		return (self.min_damage + self.max_damage) / 2
+
+	@property	
+	def stat_page(self):
+		desc = f'**DPS:** {round(self.avg_dmg, 2)}\n\n'
+		desc += f'**Damage:** {self.min_damage} - {self.max_damage}\n'
+		desc += f'**Main Stat:** {self.stat}\n\n'
+		return Page(self.name, desc, colour=(22, 0, 240))
