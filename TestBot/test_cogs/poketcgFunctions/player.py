79a78595b395158f1c36282d3cc05be5a8d55b4e
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-08-14 11:08:24 -0400

    Player functions

--- TestBot/test_cogs/poketcgFunctions/player.py
+++ TestBot/test_cogs/poketcgFunctions/player.py
@@ -1,10 +1,26 @@
-from .. import Page
+from .. import Page, sql
+from datetime import datetime as dt
 
 def get_player(discord_id):
-	...
+	df = sql('poketcg', 'select * from players where discord_id = ?', (discord_id,))
+	if df.empty:
+		return add_player(discord_id)
+	return Player(**df.to_dict('records')[0])
 
 def add_player(discord_id):
-	...
+	player = Player(
+		discord_id,
+		25,
+		dt.now(),
+		{},
+		0,
+		0,
+		25,
+		0,
+		0
+	)
+	sql('poketcg', 'insert into players values (?,?,?,?,?,?,?,?,?)', player.creation_row)
+	return Player
 
 class Player:
 	def __init__(
@@ -21,10 +37,24 @@ class Player:
 	):
 		self.discord_id = discord_id
 		self.cash = cash
-		self.daily_reset = daily_reset
-		self.packs = packs
+		self.daily_reset = daily_reset if isinstance(daily_reset, dt) else dt.fromtimestamp(daily_reset)
+		self.packs = packs if isinstance(packs, dict) else json.loads(packs)
 		self.packs_opened = packs_opened
 		self.packs_bought = packs_bought
 		self.total_cash = total_cash
 		self.total_cards = total_cards
 		self.cards_sold = cards_sold
+
+	@property
+	def creation_row(self):
+		return (
+			self.discord_id,
+			self.cash,
+			self.daily_reset.timestamp()
+			json.dumps(self.packs),
+			self.packs_opened,
+			self.packs_bought,
+			self.total_cash,
+			self.total_cards,
+			self.cards_sold
+		)
